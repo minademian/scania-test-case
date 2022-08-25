604890fdddce839639ad455f56c63dd74c260390
Mina Demian <mina@minademian.com>
2022-08-25 23:19:53 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'scania-test-case';
+}
